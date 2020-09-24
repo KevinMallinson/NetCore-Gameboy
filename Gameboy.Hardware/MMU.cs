a4723843d3ffa879b5c053cdbd79c864648ac0b5
Kevin Mallinson <kevin.mallinson@outlook.com>
2020-09-24 15:29:13 -0400

    WIP

--- Gameboy.Hardware/MMU.cs
+++ Gameboy.Hardware/MMU.cs
@@ -103,17 +103,9 @@ namespace Gameboy.Hardware
         private GBMemory? SetOrGetMemory(ushort addr, byte? val = null)
         {
             //Using memory map at http://gbdev.gg8.se/wiki/articles/Memory_Map
-            switch (addr & 0xF000)
+            switch (addr)
             {
-
-                case 0x0000:
-                case 0x1000:
-                case 0x2000:
-                case 0x3000: //16KB ROM bank 00
-                case 0x4000:
-                case 0x5000:
-                case 0x6000:
-                case 0x7000: //16KB ROM Bank 01~NN
+                case { } when addr <= 0x7FFF: //ROM Bank
                     if (val == null)
                     {
                         return new GBMemory(MemoryRegion.ROM_BANK, _romBank[addr], addr);
@@ -123,8 +115,7 @@ namespace Gameboy.Hardware
                         _romBank[addr] = val.Value;
                         return null;
                     }
-                case 0x8000: //4kb VRAM
-                case 0x9000: //4kb VRAM - total 8kb
+                case { } when addr >= 0x8000 && addr <= 0x9FFF: //VRAM
 
                     if (val == null)
                     {
@@ -132,16 +123,11 @@ namespace Gameboy.Hardware
                     }
                     else
                     {
-                        _bus.GetGPU().SetByte(addr, (byte)val, MemoryRegion.VIDEO_RAM);
+                        _bus.GetGPU().SetByte(addr, (byte) val, MemoryRegion.VIDEO_RAM);
                         return null;
                     }
-
-                case 0xA000: //4kb external ram
-                case 0xB000: //4kb external ram - total 8kb
-
-                    //Note: ExternalRam has a range of 0x2000. Of course, 0xA000 or 0xB000 is too large. It is out of range.
-                    //Let's say we receive 0xBFFF. This should be the last element of the array. Of course, it's out of range.
-                    //We need to apply a bitmask to reduce the value. In this case, ignoring the first 3 bits serves this purpose.
+                case { } when addr >= 0xA000 && addr <= 0xBFFF: // External RAM
+                    //We need to apply a bitmask to reduce the value. Ignore the first 3 bits.
                     if (val == null)
                     {
                         return new GBMemory(MemoryRegion.EXTERNAL_RAM, _externalRam[addr & 0x1FFF], addr);
@@ -151,10 +137,7 @@ namespace Gameboy.Hardware
                         _externalRam[addr & 0x1FFF] = val.Value;
                         return null;
                     }
-
-                case 0xC000: //4kb work ram bank 0
-                case 0xD000: //4kb work ram bank 1-N - total 8kb
-
+                case { } when addr >= 0xC000 && addr <= 0xDFFF: // Work RAM
                     if (val == null)
                     {
                         return new GBMemory(MemoryRegion.WORK_RAM, _workRam[addr & 0x1FFF], addr);
@@ -165,8 +148,7 @@ namespace Gameboy.Hardware
                         return null;
                     }
 
-                case 0xE000: //7.5kb echo ram. Only part, rest in 0xF000
-
+                case { } when addr >= 0xE000 && addr <= 0xFDFF: // Echo RAM
                     if (val == null)
                     {
                         return new GBMemory(MemoryRegion.ECHO_RAM, _workRam[addr & 0x1FFF], addr);
@@ -176,158 +158,61 @@ namespace Gameboy.Hardware
                         _workRam[addr & 0x1FFF] = val.Value;
                         return null;
                     }
-
-                case 0xF000:
-                    switch (addr & 0x0F00)
+                case { } when addr >= 0xFE00 && addr <= 0xFE9F: // Sprite attribute table (OAM)
+                    if (val == null)
                     {
-
-                        case 0x000:
-                        case 0x100:
-                        case 0x200:
-                        case 0x300:
-                        case 0x400: //this is the rest of the echo ram
-                        case 0x500:
-                        case 0x600:
-                        case 0x700:
-                        case 0x800:
-                        case 0x900: //this is the rest of the echo ram
-                        case 0xA00:
-                        case 0xB00:
-                        case 0xC00:
-                        case 0xD00: //this is the rest of the echo ram
-
-                            if (val == null)
-                            {
-                                return new GBMemory(MemoryRegion.ECHO_RAM, _workRam[addr & 0x1FFF], addr);
-                            }
-                            else
-                            {
-                                _workRam[addr & 0x1FFF] = val.Value;
-                                return null;
-                            }
-
-                        case 0xE00:
-
-                            switch (addr & 0x00F0)
-                            {
-
-                                case 0x00:
-                                case 0x10:
-                                case 0x20:
-                                case 0x30:
-                                case 0x40: //Sprite attribute table (OAM)
-                                case 0x50:
-                                case 0x60:
-                                case 0x70:
-                                case 0x80:
-                                case 0x90: //Sprite attribute table (OAM)
-
-                                    if (val == null)
-                                    {
-                                        return _bus.GetGPU().GetByte(addr, MemoryRegion.SPRITE_ATTRIBUTE_TABLE);
-                                    }
-                                    else
-                                    {
-                                        _bus.GetGPU().SetByte(addr, (byte)val, MemoryRegion.SPRITE_ATTRIBUTE_TABLE);
-                                        return null;
-                                    }
-
-                                case 0xA0:
-                                case 0xB0:
-                                case 0xC0: //Not Usable
-                                case 0xD0:
-                                case 0xE0:
-                                case 0xF0: //Not Usable
-                                    return new GBMemory(MemoryRegion.UNUSED, 0, addr);
-                            }
-
-                            break;
-
-                        case 0xF00:
-
-                            switch (addr & 0x00F0)
-                            {
-                                case 0x00:
-                                case 0x10:
-                                case 0x20:
-                                case 0x30: //IO Registers
-                                case 0x40:
-                                case 0x50:
-                                case 0x60:
-                                case 0x70: //IO Registers
-
-                                    if (val == null)
-                                    {
-                                        return new GBMemory(MemoryRegion.IO_REGISTERS, _ioRegisters[addr & 0x007F], addr);
-                                    }
-                                    else
-                                    {
-                                        _ioRegisters[addr & 0x007F] = val.Value;
-                                        return null;
-                                    }
-
-                                case 0x80:
-                                case 0x90:
-                                case 0xA0:
-                                case 0xB0: //High RAM (HRAM)
-                                case 0xC0:
-                                case 0xD0:
-                                case 0xE0: //High RAM (HRAM)
-
-                                    if (val == null)
-                                    {
-                                        return new GBMemory(MemoryRegion.HIGH_RAM, _highRam[addr & 0x007F], addr);
-                                    }
-                                    else
-                                    {
-                                        _highRam[addr & 0x007F] = val.Value;
-                                        return null;
-                                    }
-
-                                case 0xF0:
-                                    //if the last nibble is not F, it is still high ram, else it's the interrupts enable register
-                                    if ((addr & 0x000F) == 0x000F)
-                                    {
-                                        if (val == null)
-                                        {
-                                            return new GBMemory(MemoryRegion.INTERRUPT_FLAG,
-                                                (ushort)(_interruptEnableFlag ? 1 : 0), addr);
-                                        }
-                                        else
-                                        {
-                                            if (val.Value > 1)
-                                            {
-                                                throw new Exception($"Interrupt Enable Flag can only be 0 or 1. Passed value: {val.Value}");
-                                            }
-                                            
-                                            _interruptEnableFlag = val.Value == 1;
-                                            return null;
-                                        }
-                                    }
-                                    else
-                                    {
-                                        if (val == null)
-                                        {
-                                            return new GBMemory(MemoryRegion.HIGH_RAM, _highRam[addr & 0x007F], addr);
-                                        }
-                                        else
-                                        {
-                                            _highRam[addr & 0x007F] = val.Value;
-                                            return null;
-                                        }
-                                    }
-
-                            }
-
-                            break;
-
+                        return _bus.GetGPU().GetByte(addr, MemoryRegion.SPRITE_ATTRIBUTE_TABLE);
                     }
+                    else
+                    {
+                        _bus.GetGPU().SetByte(addr, (byte) val, MemoryRegion.SPRITE_ATTRIBUTE_TABLE);
+                        return null;
+                    }
+                case { } when addr >= 0xFEA0 && addr <= 0xFEFF: //Not Usable
+                    if (val == null)
+                    {
+                        return new GBMemory(MemoryRegion.UNUSED, 0, addr);
+                    }
+                    else
+                    {
+                        return null;
+                    }
+                case { } when addr >= 0xFF00 && addr <= 0xFF7F: //IO Registers
+                    if (val == null)
+                    {
+                        return new GBMemory(MemoryRegion.IO_REGISTERS, _ioRegisters[addr & 0x007F], addr);
+                    }
+                    else
+                    {
+                        _ioRegisters[addr & 0x007F] = val.Value;
+                        return null;
+                    }
+                case { } when addr >= 0xFF80 && addr <= 0xFFFE: //High RAM (HRAM)
 
-                    break;
-
+                    if (val == null)
+                    {
+                        return new GBMemory(MemoryRegion.HIGH_RAM, _highRam[addr & 0x007F], addr);
+                    }
+                    else
+                    {
+                        _highRam[addr & 0x007F] = val.Value;
+                        return null;
+                    }
+                case { } when addr == 0xFFFF: //Interrupt Enable Flag
+                    if (val == null)
+                    {
+                        return new GBMemory(MemoryRegion.INTERRUPT_FLAG, (ushort) (_interruptEnableFlag ? 1 : 0), addr);
+                    }
+                    else
+                    {
+                        _interruptEnableFlag = val.Value <= 1
+                            ? Convert.ToBoolean(val.Value)
+                            : throw new Exception($"Interrupt Enable Flag can only be 0 or 1. Passed value: {val.Value}");
+                        return null;
+                    }
             }
-
-            throw new Exception($"Unable to find the memory address {addr}");
+            
+            throw new Exception("A catastrophic error has occured in the MMU.");
         }
 
         public string Dump()
